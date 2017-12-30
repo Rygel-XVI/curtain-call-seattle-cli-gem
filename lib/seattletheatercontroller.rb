05360bbdc74bddfca379800601cac79048c4c828
Annette <micha232@umn.edu>
2017-12-30 09:23:37 +0000

    fixed environment and initial case statement, added more pseudocode/plans

--- lib/seattletheatercontroller.rb
+++ lib/seattletheatercontroller.rb
@@ -1,34 +1,48 @@
+
+require 'pry'
 class SeattleTheaterController
    
    def call
+       #1. scrapes the websites
+       #2. make objects from those websites
+       #3. Class show has  :theaters, :genres, :dates  the show cannot change these
+       #4. Class theater has :shows, :dates, :location
+       #5. 
+       
+       
       # asks if you would like shows by theater, date/time, genre
       puts "Would you like shows by theater, genre, or date?"
       input = gets.chomp
       #gets input and calls method mased on input
-      
+
       case input
-      when input =~ /theater/i
+      when /theater/i
           shows_by_theater
-      when input =~ /genre/i
+      when /genre/i
           shows_by_genre
-      when input =~ /date/i
+      when /date/i
           shows_by_date
       end
       
-      #ask for a speific show and then list the show, it's genre, the dates it is play for each theater it is playing at.
+      #ask for a specific show and then list the show, it's genre, the dates it is playing at for each theater.
+      #also puts a description of the show
       
    end
 
+    #return shows playing at each theater
     def shows_by_theater
-        
+        puts "shows_by_theater"
     end
     
+    #return shows playing on a specific date (or maybe in order by soonest)
     def shows_by_date
-        
+        puts "shows_by_date"
     end
     
+    #return shows by genre (kids, musical, play, classic, monologue, other)
     def shows_by_genre
-        
+        puts "shows_by_genre"
     end
+    
     
 end
--- plan.md
+++ plan.md
@@ -10,7 +10,7 @@ https://www.villagetheatre.org/   everett and issaquah locations
 can filter what's playing by
 musicals
 plays
-childrens
+kids
 
 can filter by theater
 
