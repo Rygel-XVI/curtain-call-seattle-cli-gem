7a0d65bc93d92da4801f815215fcb3106f9dda1b
Rygel-XVI <micha232@umn.edu>
2019-04-08 18:40:46 -0700

    updated initial scrape for SCT to get current season of shows url

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ class CurtainCallSeattle::Scraper
     end
 
     begin
-      sct = self.scrape_childrens('http://www.sct.org/shows/')
+      sct = self.scrape_childrens('http://www.sct.org/onstage/')
       CurtainCallSeattle::Show.create_shows_array(sct)
     rescue
       puts "Childrens Theater is broken. Please open issue at https://github.com/Rygel-XVI/curtain-call-seattle-cli-gem/issues"
@@ -65,13 +65,19 @@ class CurtainCallSeattle::Scraper
 
 ## add other info from website in the future (ie shows with interpreters, age recommendations)
     def self.scrape_childrens(url)
+      begin
         doc = Nokogiri::HTML(open(url))
-        a = doc.css("div.left-column ul.left-nav a")[1]['href']
+        a = doc.css("ul#menu-main-navigation li a")[0]["href"]
         shows_sct('http://www.sct.org/' + a)
+      rescue
+        puts "scrape childrens not executing"
+      end
     end
 
     def self.shows_sct(url)
+      begin
         doc = Nokogiri::HTML(open(url))
+        binding.pry
         a = doc.css("div.content table.pagelist tr td")
 
         sct = CurtainCallSeattle::Theater.new
@@ -83,25 +89,37 @@ class CurtainCallSeattle::Scraper
                                         :theater => sct,
                                         :description => parse_description_childrens(i)[1]}
         }
+      rescue
+        puts "shows_sct not functioning"
+      end
     end
 
     def self.parse_description_childrens(i)
+      begin
         i = i.css("p").text
         i.gsub!(/\t|\n|\r/, "")
         i.split(/\s{2,}/)
+      rescue
+        puts "sct descriptions not functioning"
+      end
     end
 
     def self.create_dates_childrens(i)
+      begin
         d = i.css("p b")[1].text
         d = d.split (/–|,\s/)  ##this is a special dash of some sort it is not a hyphen if you delete this copy paste --> –
         dates = [d[0] + " " + d[2], d[1] + " " + d[2]]
         y = dates.map {|x| Date.parse(x)}
         (y[0]...y[1])
+      rescue
+        puts "sct date creation not funcitoning"
+      end
     end
 
 
 ###Scraper for Paramount Theater ###
     def self.scrape_paramount(url)
+      begin
         doc = Nokogiri::HTML(open(url))
         a = doc.css("ul .result")
 
@@ -114,10 +132,13 @@ class CurtainCallSeattle::Scraper
             :theater => paramount,
             :description => get_description_paramount("https://seattle.broadway.com" + i.css("a")[1]['href'])}
         }
+      rescue
+        puts "scrape_paramount broken"
+      end
     end
 
     def self.create_dates_paramount(i)
-
+      begin
         b = i.css("p.dates")
         b.at_css("span").remove if b.at_css("span")
         b = b.text
@@ -135,10 +156,17 @@ class CurtainCallSeattle::Scraper
 
         y = dates.map {|x| Date.parse(x)}
         (y[0]...y[1])
+      rescue
+        puts "dates_paramount not working"
+      end
     end
 
     def self.get_description_paramount(url)
+      begin
         doc = Nokogiri::HTML(open(url))
         doc.css("div.mod-story p").text
+      rescue
+        puts "description_paramount not working"
+      end
     end
 end
